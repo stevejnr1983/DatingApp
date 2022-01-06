925b7d21558a0ecb565ac87b3006743d348259f2
stevejnr1983 <s_cot1983@hotmail.co.uk>
2022-01-06 21:09:03 +0000

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<AppUser> Users { get; set; }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
