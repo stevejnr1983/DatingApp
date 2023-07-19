ce5829bd60ac2a3eb13a551b57f1691087569223
stevejnr1983 <s_cot1983@hotmail.co.uk>
2023-07-19 21:21:40 +0100

    End of Section 14

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,8 +10,30 @@ namespace API.Data
     public class DataContext : DbContext
     {
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
         public DataContext(DbContextOptions options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                .HasKey(k => new {k.SourceUserId, k.TargetUserId});
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+       
+            builder.Entity<UserLike>()
+                .HasOne(s => s.TargetUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.TargetUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+                
         }
     }
 }
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -90,6 +90,14 @@ export class MembersService {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string) {
+    return this.http.get<Member[]>(this.baseUrl + 'likes?predicate=' + predicate);
+  }
+
   setMainPhoto(photoId: number) {
     return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
   }
